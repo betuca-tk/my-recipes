2a370142f3455046a064557eada057665072eda4
Carlos <carlos.amorim@travelperk.com>
2024-01-30 17:55:41 +0100

    Add unit test for list recipes

--- app/recipes_book/tests/test_recipe_api.py
+++ app/recipes_book/tests/test_recipe_api.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from recipes_book.models import Recipe
+from recipes_book.models import Recipe, Ingredient
 import json
 
 
@@ -51,3 +51,38 @@ class RecipeTest(TestCase):
         saved_recipe = Recipe.objects.get(name=posta_data["name"])
         self.assertEqual(saved_recipe.description, posta_data["description"])
         self.assertEqual(saved_recipe.ingredients.count(), 2)
+
+    def test_get_recipes(self):
+        """
+        Test GET response for recipe list
+        """
+        recipe1 = Recipe.objects.create(
+            name="Recipe 1",
+            description="Recipe 1 description",
+        )
+        ingredient1 = Ingredient.objects.create(name="Ingredient 1")
+        ingredient2 = Ingredient.objects.create(name="Ingredient 2")
+        recipe1.ingredients.add(ingredient1)
+        recipe1.ingredients.add(ingredient2)
+
+        recipe2 = Recipe.objects.create(
+            name="Recipe 2",
+            description="Recipe 2 description",
+        )
+
+        response = self.client.get("/recipes/")
+        self.assertEqual(response.status_code, 200)
+
+        response_data = response.json()
+        self.assertEqual(len(response_data), 2)
+
+        recipe1_data = response_data[0]
+        self.assertEqual(recipe1_data["name"], recipe1.name)
+        self.assertEqual(recipe1_data["description"], recipe1.description)
+        self.assertEqual(len(recipe1_data["ingredients"]), 2)
+        self.assertEqual(recipe1_data["ingredients"][0]["name"], "Ingredient 1")
+        self.assertEqual(recipe1_data["ingredients"][1]["name"], "Ingredient 2")
+
+        recipe2_data = response_data[1]
+        self.assertEqual(recipe2_data["name"], recipe2.name)
+        self.assertEqual(recipe2_data["description"], recipe2.description)
